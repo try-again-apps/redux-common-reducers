3d24c7b90dc01269acf6c60b3208c43ac2f00574
Try Again Apps <try.again.apps@gmail.com>
2016-11-30 22:02:28 +0100

    Add stringReducer tests

---
+++ __tests__/stringReducer-test.js
@@ -0,0 +1,51 @@
+import _sample from 'lodash/sample';
+import { stringReducer } from '../src';
+
+const changeActions = ['CHANGE_ACTION1', 'CHANGE_ACTION2', 'CHANGE_ACTION3'];
+const resetActions = ['RESET_ACTION1', 'RESET_ACTION2'];
+const unknownAction = 'UNKNOWN_ACTION';
+const currentState = 'reduce me!';
+
+describe("stringReducer", () => {
+  describe("no initial state, unknown action", () => {
+    it("returns current state if initial not set", () => {
+      const reducer = stringReducer(changeActions, resetActions, ['path']);
+      const action = { type: unknownAction, payload: { path: 'reduced by me!' } };
+      expect(reducer(currentState, action)).toBe(currentState);
+    });
+  });
+
+  describe("initial state set, unknown action", () => {
+    it("returns initial state when set", () => {
+      const reducer = stringReducer(changeActions, resetActions, ['path'], 'initialize me!');
+      const action = { type: unknownAction, payload: { path: 'reduced by me!' } };
+      expect(reducer(currentState, action)).toBe('initialize me!');
+    });
+  });
+
+  describe("change actions", () => {
+    it("returns value from action, initial state not set", () => {
+      const reducer = stringReducer(changeActions, resetActions, ['path']);
+      const action = { type: _sample(changeActions), payload: { path: 'reduced by me!' } };
+      expect(reducer(currentState, action)).toBe('reduced by me!');
+    });
+    it("returns value from action, initial state set", () => {
+      const reducer = stringReducer(changeActions, resetActions, ['path'], 'initialize me!');
+      const action = { type: _sample(changeActions), payload: { path: 'reduced by me!' } };
+      expect(reducer(currentState, action)).toBe('reduced by me!');
+    });
+  });
+
+  describe("reset actions", () => {
+    it("returns '' value, initial state not set, payload ignored", () => {
+      const reducer = stringReducer(changeActions, resetActions, ['path']);
+      const action = { type: _sample(resetActions), payload: { path: 'reduced by me!' } };
+      expect(reducer(currentState, action)).toBe('');
+    });
+    it("returns value from action, initial state set, payload ignored", () => {
+      const reducer = stringReducer(changeActions, resetActions, ['path'], 'initialize me!');
+      const action = { type: _sample(resetActions), payload: { path: 'reduced by me!' } };
+      expect(reducer(currentState, action)).toBe('');
+    });
+  });
+});
--- src/stringReducer.js
+++ src/stringReducer.js
@@ -1,7 +1,11 @@
 import _get from 'lodash/get';
 
-export default function stringReducer(changeActions, resetActions, path) {
-  return (state = '', action) => {
+export default function stringReducer(changeActions, resetActions, path, initialState) {
+  return (state, action) => {
+    if (initialState !== undefined) {
+      state = initialState;
+    }
+
     if (~changeActions.indexOf(action.type)) {
       return _get(action.payload, path);
     }
