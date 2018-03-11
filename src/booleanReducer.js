cab75e191f05023a7a50a6287d11624b14afec32
Chris Trevino <darthtrevino@gmail.com>
2018-03-11 12:11:21 -0700

    Use eslint-config-prettier to remove style-rules
    Since prettier is automatically handling style, eslint does not need to check style-centric rules anymore. Using tabs for spacing since it looks like "smart-tabs" was used in the eslint ruleset.

--- src/booleanReducer.js
+++ src/booleanReducer.js
@@ -1,20 +1,20 @@
 export default function booleanReducer(
-  { trueActions = [], falseActions = [], toggleActions = [] },
-  initialValue = false
+	{ trueActions = [], falseActions = [], toggleActions = [] },
+	initialValue = false
 ) {
-  if (typeof initialValue !== 'boolean') {
-    throw new TypeError('Initial state needs to be boolean');
-  }
+	if (typeof initialValue !== 'boolean') {
+		throw new TypeError('Initial state needs to be boolean');
+	}
 
-  return (state = initialValue, action) => {
-    if (~trueActions.indexOf(action.type)) {
-      return true;
-    } else if (~falseActions.indexOf(action.type)) {
-      return false;
-    } else if (~toggleActions.indexOf(action.type)) {
-      return !state;
-    }
+	return (state = initialValue, action) => {
+		if (~trueActions.indexOf(action.type)) {
+			return true;
+		} else if (~falseActions.indexOf(action.type)) {
+			return false;
+		} else if (~toggleActions.indexOf(action.type)) {
+			return !state;
+		}
 
-    return state;
-  };
+		return state;
+	};
 }
--- src/stringReducer.js
+++ src/stringReducer.js
@@ -2,26 +2,26 @@ import _get from 'lodash/get';
 import _has from 'lodash/has';
 
 export default function stringReducer(
-  { changeActions, resetActions, path },
-  initialValue = ''
+	{ changeActions, resetActions, path },
+	initialValue = ''
 ) {
-  if (typeof initialValue !== 'string') {
-    throw new TypeError('Initial state needs to be string');
-  }
+	if (typeof initialValue !== 'string') {
+		throw new TypeError('Initial state needs to be string');
+	}
 
-  return (state = initialValue, action) => {
-    if (~changeActions.indexOf(action.type)) {
-      if (!_has(action, path)) {
-        throw new Error(`Path "${path.join('.')}" not found in action!`);
-      }
+	return (state = initialValue, action) => {
+		if (~changeActions.indexOf(action.type)) {
+			if (!_has(action, path)) {
+				throw new Error(`Path "${path.join('.')}" not found in action!`);
+			}
 
-      return _get(action, path);
-    }
+			return _get(action, path);
+		}
 
-    if (~resetActions.indexOf(action.type)) {
-      return initialValue;
-    }
+		if (~resetActions.indexOf(action.type)) {
+			return initialValue;
+		}
 
-    return state;
-  };
+		return state;
+	};
 }
